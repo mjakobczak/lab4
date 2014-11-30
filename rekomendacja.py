d150b295e3c9e0ee2461b0592896e0b2cb608bfb
mjakobczak <jakobczakmarta@gmail.com>
2014-11-30 23:24:48 +0100

    lab4_MED

--- rekomendacja.py
+++ rekomendacja.py
@@ -2,15 +2,16 @@
 #
 
 from math import sqrt
+import numpy
 
 users = {
         "Ania": 
-            {"Blues Traveler": 1.,
+            {"Blues Traveler": 1.0,
             "Broken Bells": 1.5,
             "Norah Jones": 2,
             "Deadmau5": 2.5,
             "Phoenix": 3.0,
-            "Slightly Stoopid": .5,
+            "Slightly Stoopid": 0.5,
             "The Strokes": 0.0,
             "Vampire Weekend": 2.0},
          "Bonia":
@@ -41,18 +42,53 @@ def manhattan(rating1, rating2):
         if klucz in rating2.keys():
             udaloSiePorownac = True
             odleglosc = odleglosc + abs(rating2[klucz] - rating1[klucz])
-
+    
     if (udaloSiePorownac==True):
+        print "Odleglosc Manhattan:"
+        print odleglosc
         return odleglosc
     else:
         return -1
 
 def pearson(rating1, rating2):
+    klucze1 = rating1.keys()
+    klucze2 = rating2.keys()
     korelacja=0
-    
+    sumaXY=0
+    sumaX=0
+    sumaY=0
+    n=0
+    sumaXX=0
+    sumaYY=0
+
+    for klucz in klucze1:
+        if klucz in rating2.keys():
+            
+            sumaXY= sumaXY + rating1[klucz]*rating2[klucz]
+            sumaX= sumaX+ rating1[klucz]
+            sumaY= sumaY+ rating2[klucz]
+            n= n+1
+            sumaXX= sumaXX + rating1[klucz]*rating1[klucz]
+            sumaYY= sumaYY + rating2[klucz]*rating2[klucz]
+
+            
+    licznik= sumaXY- sumaX*sumaY/n
+    mianownik= sqrt(sumaXX- sumaX*sumaX/n) * sqrt(sumaYY-sumaY*sumaY/n)
+    korelacja= licznik/mianownik
+    # korelacja=0.98 -> silna korelacja
+    print "Korelacja"
+    print korelacja
     return korelacja
 
+
 def pearsonNumpy(rating1, rating2):
-    
     korelacja=0
+    keys = list(rating1.viewkeys() | rating2.viewkeys()) # Lista ze wszystkimi kluczami z obu słowników
+    korelacja=numpy.corrcoef([rating1[x] for x in keys], [rating2[x] for x in keys])[0][1]
+    print "Korelacja Numpy"
+    print korelacja
     return korelacja
+
+pearson(users["Ania"], users["Bonia"])
+manhattan(users["Ania"], users["Bonia"])
+pearsonNumpy(users["Ania"], users["Bonia"])
